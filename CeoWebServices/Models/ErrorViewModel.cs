4c3d6b710979ba081bdb514c0c607971d4d7486b
gjaimes <gjaimes@emeingeniria.com.co>
2018-10-17 14:40:34 -0500

    Agregar archivos de proyecto.

---
+++ CeoWebServices/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CeoWebServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
